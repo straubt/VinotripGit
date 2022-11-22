6ed8fd37497b4eb7150791dccfbc8f9313a4b927
straubt <titouan.straub@etu.univ-smb.fr>
2022-11-22 14:07:05 +0000

    clean main

--- vinotripS232/resources/views/welcome.blade.php
+++ vinotripS232/resources/views/welcome.blade.php
@@ -10,34 +10,6 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/caroussel.css">
 </head>
-<<<<<<< HEAD
-<header>
-    <img id="logoSlogan" src="images/logo + slogan vinotrip.PNG">
-    <button id="route_des_vins" onclick="location.href='route-des-vins'">Route des vins</button>
-    <button id="registerButton" onclick="location.href='register'">S'inscrire</button>
-</header>
-<body>
-<div id="slider">
-        <ul id="slideWrap">
-            @foreach ($sejour as $unSejour)
-            <li><img src="{{$unSejour['photo_sejour']}}" alt=""></li>
-            @endforeach 
-        </ul>   
-        <a id="prev" href="#">&#8810;</a>
-        <a id="next" href="#">&#8811;</a>
-</div>
-    <br>
-
-    <section class="bandeau"> 
-        <br>
-        <p class="mentionl"> Nous utilisons des cookies et d'autres technologies qui sont indispensables pour vous fournir les 
-            services et les fonctionnalités du site conformément à notre Avis sur les cookies. Si vous acceptez et cliquez sur 
-            Tout accepter, nous autoriserons également des sociétés tierces partenaires à stocker des cookies sur votre appareil 
-            et à utiliser des technologies similaires pour collecter et utiliser vos données personnelles (par exemple, l'adresse
-             IP) à des fins de personnalisation des publicités, de mesure et d'analyse sur nos sites et en dehors. Vous pouvez 
-             refuser votre consentement en cliquant sur Tout refuser ou faire des choix précis en sélectionnant Personnaliser 
-             mes choix. Vous pouvez retirer votre consentement à tout moment sur la page https://nomDuSite.fr </p>  
-=======
 <body>
     <script>
         var data = <?php echo json_encode($sejour);?>;
